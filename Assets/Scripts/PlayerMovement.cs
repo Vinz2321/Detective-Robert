43849a9de7bbecfa9c6cc755ebbafdc7834389cc
vnzntt <vinzentmsanchez@gmail.com>
2024-11-07 10:38:08 +0800

    Added by vin 2
    added sprint, stamina and stamina bar

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,31 +1,97 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-     public JoystickScript joystickScript;
+    public JoystickScript joystickScript;
     public float moveSpeed = 5f;
-
+    public float boostMultiplier = 2f; // Speed boost factor
     public Rigidbody2D rb;
     public Animator animator;
+    public Slider staminaSlider;
 
-    Vector2 movement;
+    // Stamina variables
+    public float maxStamina = 150f;
+    public float staminaDrainRate = 50f;    // Stamina drain per second while boosting
+    public float staminaRegenRate = 25f;    // Stamina regen per second
+    private float currentStamina;
 
-    // Update is called once per frame
-    void Update()
+    
+    private bool isBoosting = false;
+
+    private Vector2 movement;
+
+    void Start()
     {
-        movement.x = joystickScript.joystickVec.x;
-        movement.y = joystickScript.joystickVec.y;
+        // Initialize current stamina to maximum
+        currentStamina = maxStamina;
 
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (staminaSlider != null)
+        {
+            staminaSlider.maxValue = maxStamina;
+            staminaSlider.value = currentStamina; // Set slider value to match current stamina
+        }
     }
 
-     void FixedUpdate() 
-    
+    void Update()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Update movement based on joystick input
+        if (joystickScript != null)
+        {
+            movement.x = Input.GetAxisRaw("Horizontal");
+            movement.y = Input.GetAxisRaw("Vertical");
+
+            animator.SetFloat("Horizontal", movement.x);
+            animator.SetFloat("Vertical", movement.y);
+            animator.SetFloat("Speed", movement.sqrMagnitude);
+        }
+
+        // Drain stamina if boosting
+        if (isBoosting && currentStamina > 0)
+        {
+            currentStamina -= staminaDrainRate * Time.deltaTime;
+            if (currentStamina <= 0)
+            {
+                currentStamina = 0;
+                StopBoost(); // Stop boost if stamina is depleted
+            }
+        }
+        else
+        {
+            // Regenerate stamina when not boosting
+            currentStamina = Mathf.Min(currentStamina + staminaRegenRate * Time.deltaTime, maxStamina);
+        }
+
+        if (staminaSlider != null)
+        {
+            staminaSlider.value = currentStamina;
+        }
+    }
+
+    void FixedUpdate() 
+    {
+        if (rb != null)
+        {
+            // Apply speed boost if boosting and stamina is available
+            float currentSpeed = isBoosting ? moveSpeed * boostMultiplier : moveSpeed;
+            rb.MovePosition(rb.position + movement * currentSpeed * Time.fixedDeltaTime);
+        }
+    }
+
+    // Method to start boost (call this from the UI button)
+    public void StartBoost()
+    {
+        if (currentStamina > 0) // Start boost only if there is stamina left
+        {
+            isBoosting = true;
+        }
+    }
+
+    // Method to stop boost (call this from the UI button)
+    public void StopBoost()
+    {
+        isBoosting = false;
     }
 }
